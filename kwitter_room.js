650e827f66b0fe709d72a78a788b245fab4b79af
ProCoderAyaan <95852791+ProCoderAyaan@users.noreply.github.com>
2022-08-18 14:22:27 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,5 +1,3 @@
-
-//ADD YOUR FIREBASE LINKS HERE
 user_name = localStorage.getItem("user_name");
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "! ";
 
@@ -10,8 +8,6 @@ function getData() {
       firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";
 snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
-      //Start code
-
-      //End code
+      
       });});}
 getData();
